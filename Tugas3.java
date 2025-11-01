3ff99466339e040ef749e49c374048aa76f31d1a
Vallent12 <fevadtya09.gmail.com>
2025-11-01 17:28:23 +0700

    Jawaban3

---
+++ Tugas3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Tugas3 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Jumlah cabang kafe: ");
+        int jumlahCabang = sc.nextInt();
+
+        int totalPelanggan = 0;
+        int totalItem = 0;
+
+        System.out.println("\n === Input Penjualan Per Cabang ===");
+
+        for (int i = 1; i <= jumlahCabang; i++) {
+            System.out.println("\n-- Cabang " + i + " --");
+            System.out.print("Jumlah pelanggan: ");
+            int jumlahPelanggan = sc.nextInt();
+
+            totalPelanggan += jumlahPelanggan;
+            int totalItemCabang = 0;
+
+            for (int j = 1; j <=jumlahPelanggan; j++) {
+                System.out.print("- Pelanggan: " + j + " memesan berapa item? " );
+                int item = sc.nextInt();
+                totalItemCabang += item;
+            }
+
+            totalItem += totalItemCabang;
+
+            System.out.println("Cabang " + i + ":");
+            System.out.println("- Pelanggan: " + jumlahPelanggan + " orang");
+            System.out.println("- Item terjual: " + totalItemCabang);
+        }
+
+        System.out.println("\nTotal Seluruh Cabang:");
+        System.out.println("Pelanggan: " + totalPelanggan + " orang");
+        System.out.println("Item terjual: " + totalItem + " item");
+
+        sc.close();
+
+    }
+}
